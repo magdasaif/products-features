4e0a2961ffad357ee837510455c8ab3f324128f6
magda <magdasaif3@gmail.com>
2024-08-19 16:49:38 +0300

    start product package

---
+++ src/app/providers/ProductServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+namespace Magdasaif\Products\app\providers; 
+
+use Illuminate\Support\ServiceProvider;
+
+class ProductServiceProvider extends ServiceProvider
+{
+    public function boot(){
+        //==============================================================================================
+        //to load package routes in any project background
+        $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
+
+        //to load package migrations files in any project background
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+
+        //to handle publish step for migrations file 
+        $this->publishes([
+            __DIR__.'/../../database/migrations' => database_path('migrations')
+        ], 'product-migrations');
+        //==============================================================================================
+    }
+
+    public function register(){
+        
+    }
+}
